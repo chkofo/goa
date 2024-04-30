477d022e0d580757cae4e7880b9fd4c378dc8741
chkofo <giorgichkofoia91@gmail.com>
2024-04-30 18:28:39 +0400

    first commit

---
+++ day 1/claswork/lesson.py
@@ -0,0 +1,35 @@
+name = "giorgi"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "giorgi"  არის ცვლადისთის მნიშვნელობა
+
+surname= "chqopia"
+
+print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "giorgi" #ეს არის  str (string) ტიპის ცვლადი
+age = "15" #ეს არის ints ( integer ) მთელი რიცხვი
+height = "1.88" #ეს არის float ტიპის ცვლადი ( ათწილადი )
+#boolean (bool) ტიპის ცვლადი
+
+knows_programming = "true"  #true ან false
+is_ugly = "false" #snakecase (უბრალოდ წერის სტილი სტანდარტულად)
+
+isUgly = False # ჯავასქრიფტული comelcase
+
+
+print(name +"  "+ surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+# print(name + age) 
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+print (name + " " + str(age))
+
+
---
+++ day 1/homework/lesson.py
@@ -0,0 +1,5 @@
+name = "giorgi"
+surname = "chqofoia"
+age = 15
+
+print("მე ვარ" + "  " + name  +"  "+ "ჩემი გვარია" + "  " + surname + "  " + "ვარ"+  " "  + str(age) + " " +"წლის") 
