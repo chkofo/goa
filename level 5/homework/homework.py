b9da16c75edd22c550545623576ea3071807d5de
chkofo <giorgichkofoia91@gmail.com>
2024-05-27 22:51:21 +0400

    level5

---
+++ level 5/homework/homework.py
@@ -0,0 +1,43 @@
+name = "გიორგი"
+surname = "ჩქოფოია"
+age = 15
+city = "ქუთაისში"
+
+print("მე ვარ" + "  " + name  +"  "+ "ჩემი გვარია" + "  " + surname + "  " + "ვარ"+  " "  + str(age) + " " +"წლის" + " " + "ვცხოვრობ" + " " + city  ) 
+
+
+
+#in the 11 line, i am creating varieble named num1 and storing intenger 20
+num1 = 20
+#in the 13 line, i am creating varieble named num2 and storing intenger 10
+num2 = 10 
+#in the 15 line , i am printing sum of num 1 and num 2 value
+print(num1 + num2)
+
+#in the 17 line, i am creating varieble named num3 and storing intenger 2
+num3 = 2
+#in the 20 line, i am creating varieble named num4 and storing intenger 17
+num4 = 17
+#in the 22 line , i am printing sum of num 3 and num 4 value
+print(num3 * num4)
+
+#in the 25 line, i am creating varieble named num5 and storing intenger 42
+num5 = 42
+#in the 27 line, i am creating varieble named num6 and storing intenger 3
+num6 = 3
+#in the 29 line , i am printing sum of num 5 and num 6 value
+print(num5 / num6)
+
+#in the 32 line, i am creating varieble named num7 and storing intenger 12
+num7 = 12
+#in the 34 line, i am creating varieble named num8 and storing intenger 8
+num8 = 8
+#in the 36 line , i am printing sum of num 7 and num 8 value
+print(num7 - num8)
+
+#in the 39 line, i am creating varieble named num9 and storing intenger 50
+num9 = 50
+#in the 41 line, i am creating varieble named num10 and storing intenger 20
+num10 = 20
+#in the 36 line , i am printing sum of num 9 and num 10 value
+print(num9+num10)
