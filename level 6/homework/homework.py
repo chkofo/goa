5e77bd7d05a381b9074f35df94033d2f7465815b
chkofo <giorgichkofoia91@gmail.com>
2024-06-01 15:03:49 +0400

    level 6

---
+++ level 6/homework/homework.py
@@ -0,0 +1,28 @@
+#დავალება 2
+# num1 = int(input("enter your number 1:"))
+# num2 = int(input("enter your number 2:"))
+# print(num1 + num2)
+# print(num1 * num2)
+# print(num1 - num2 )
+# print(num1 / num2)
+
+#დავალება 3
+# name = input("enter your name:")
+# print(" gilocav dabadebis dges" + "  "+ name)
+
+#დავალება 4
+
+# name = input("enter your name:") 
+# surname = input("enter your surname:")
+# age = input("enter your age:")
+# print(name, surname, age)
+
+#დავალება 5
+mom = int(input("what is your age mom:"))
+dad = int(input("what is your age dad:"))
+sister = int(input("what is your age sister:"))
+brother = int(input("what is your age brother:"))
+print(mom + 25)
+print(dad + 25)
+print(sister + 25)
+print(brother + 25)
