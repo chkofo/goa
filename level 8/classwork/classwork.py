790b458e73f56a153c9814d4e9523a8aff6d04e8
chkofo <giorgichkofoia91@gmail.com>
2024-06-17 19:05:54 +0400

    level 8

---
+++ level 8/classwork/classwork.py
@@ -0,0 +1,14 @@
+password = input("enter your password : ")
+rep_password = input(("enter your password again:"))
+ 
+print(password == rep_password )
+
+num1=input("enter your name:")
+num2=input("enter your second name:")
+
+print(num1>num2)
+print(num1<num2)
+print(num1>=num2)
+print(num1<=num2)
+print(num1==num2)
+
