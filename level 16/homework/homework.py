1daeb8c54ce51159cd0398a88aa7eb151483b96b
chkofo <giorgichkofoia91@gmail.com>
2024-08-14 14:05:25 +0400

    level 21

---
+++ level 16/homework/homework.py
@@ -0,0 +1,68 @@
+#დავალება 1
+
+# numbers = [1,2,3,4,5,6]
+# last_element = numbers.pop()
+# print(last_element)
+# print(numbers)
+
+#დავალება 2
+
+# car = ["bmw" , "mercedes" , "lamborghini"]
+# first_element = car.pop(0)
+# print(first_element) 
+
+#დავალება 3
+
+# numbers = [1,2,3,4,5,6]
+# second_index = numbers.pop(2)
+# print(second_index)
+
+#დავალება 4
+
+# numbers = [1,2,3,4,5,6]
+# last_element = numbers.pop()
+# print(last_element)
+
+#count :  დავალება 1
+
+# numbers = [1,2,3,4,5,5,5,5,5,5,5,5]
+# print(numbers.count(5))
+
+
+#დავალება 2
+ 
+# item = ["a" , "b" ,"a" , "p" , "g"]
+# print(item.count("a"))
+
+#დავალება 3
+
+# item = [True , False , True , 10 > 5 , 10 < 2 ]
+# print(item.count(True))
+
+#დავალება 4 
+ 
+# numbers = [1,2,3,4,5,[3,4],6,7,[3,4]]
+# print(numbers.count([3,4]))
+
+
+#len : დავალება 1
+
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+# print(len(numbers))
+
+#დავალება 2
+
+# weekend = ["moonday", "thursday" , "wednesday" , "thursday" , "friday" , "saturday" , "sunday"]
+# print(len(weekend))
+
+
+#დავალება 3
+
+nested = [[1,2,3],[4,5],[3,4],[6,7],[3,4]]
+print(len(nested))
+
+
+
+
+
+
---
+++ level 20/homework/homework.py
@@ -0,0 +1,94 @@
+# 1. Write a function that takes a list of numbers and returns the sum of all the even numbers.
+# numbers = [1,2,3,4]
+# def sum_even(even) :
+#     sum = 0
+#     for i in even :
+#         if i % 2 == 0 :
+#             sum += i
+#     print(sum)
+
+# sum_even(numbers) 
+
+
+    
+
+
+
+
+# 2. Create a function that accepts a string and returns the string reversed without using any built-in reverse functions.
+
+# name = "giorgi"
+# def reverse_name(x) :
+#     print(x[::-1])
+
+# reverse_name(name)
+
+
+
+# 3. Implement a function that takes a number and returns the factorial of that number using recursion.
+# def fact(x) :
+#     if x == 1 :
+#         return 1
+#     return fact(x-1)* x
+
+# print(fact(5))
+    
+
+# 4. Design a function that accepts two lists and returns a list of common elements without using set operations.
+
+# numbers = [1,2,3,4,5,6]
+# numbers1 = [1,8,9,11,2,3,56,4,5]
+
+# def common_elements(x,y) :
+
+#     numbers2 = []
+#     for i in x :
+#         for o in y :
+#             if i == o :
+#                 numbers2.append(i)
+#     print(numbers2) 
+
+# common_elements(numbers,numbers1)
+
+
+# 5. Write a function that takes a string and returns the number of vowels in the string.
+
+# name = "pineapple"
+# vowels = ["a","e","i","o","u"]
+# def vow(name,vowels) :
+
+#     count = 0
+#     for i in name :
+#         for o in vowels :
+#             if i == o :
+#                 count += 1
+#     print(count)
+
+# vow(name,vowels)
+
+# 6. Create a function that accepts a list of integers and returns the list sorted in ascending order without using built-in sort functions.
+numbers1= [1,11,2,33,45,61,22,2,3]
+sorted_num = []
+def sorted(numbers) :
+    for i in sorted_num :
+
+
+
+
+
+
+
+# 7. Implement a function that takes two strings and returns True if one string is a permutation of the other.
+
+
+
+
+# 8. Create a function that takes a number and returns True if it is a prime number.
+
+
+
+
+# 9. Design a function that accepts a list of strings and returns a list with the strings sorted by their lengths.
+
+
+
